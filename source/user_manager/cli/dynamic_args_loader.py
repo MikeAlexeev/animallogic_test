816d7c7dba190c249e82bc62e54add99ad7e4aaf
mikhail alexeev <mike.42.alexeev@gmail.com>
2022-11-26 21:46:51 +0300

    refactored

--- source/user_manager/cli/dynamic_args_loader.py
+++ source/user_manager/cli/dynamic_args_loader.py
@@ -8,13 +8,14 @@ class DynamicArgsLoader:
         self._system_configuration = system_configuration
 
     def get_record_args(self) -> List[str]:
-        field_names = self._system_configuration.get_record_cls().get_option_names()
-        return [self._make_arg(field_name) for field_name in field_names]
+        return self._make_args(self._system_configuration.get_record_cls().get_option_names())
 
     def get_storage_args(self) -> List[str]:
-        field_names = self._system_configuration.get_storage_cls().get_option_names()
-        return [self._make_arg(field_name) for field_name in field_names]
+        return self._make_args(self._system_configuration.get_storage_cls().get_option_names())
 
     @staticmethod
-    def _make_arg(field_name: str) -> str:
-        return f"--{field_name.replace('_', '-')}"
+    def _make_args(field_names: List[str]) -> List[str]:
+        def _make_arg(field_name: str) -> str:
+            return f"--{field_name.replace('_', '-')}"
+
+        return [_make_arg(field_name) for field_name in field_names]
