c6ad10f284bcaedf00efdb6f3af2f2760ddfa6f2
mikhail alexeev <mike.42.alexeev@gmail.com>
2022-11-26 15:01:54 +0300

    added test for system configuration

--- tests/test_base_record_plugin.py
+++ tests/test_base_record_plugin.py
@@ -10,38 +10,44 @@ class TestRecord(BaseRecordPlugin):
     field_1: int
     field_2: str
 
-    NAME: str = 'test_record'
+    NAME: str = "test_record"
 
 
-@pytest.mark.parametrize('data_dict,expected_model', [
-    (
-        {
-            'field_1': 10,
-            'field_2': 'str_value',
-        },
-        TestRecord(field_1=10, field_2='str_value')
-    ),
-    (
-        {
-            'field_1': 10,
-            'field_2': 'str_value',
-            'field_3': 'garbage',
-        },
-        TestRecord(field_1=10, field_2='str_value')
-    )
-])
+@pytest.mark.parametrize(
+    "data_dict,expected_model",
+    [
+        (
+            {
+                "field_1": 10,
+                "field_2": "str_value",
+            },
+            TestRecord(field_1=10, field_2="str_value"),
+        ),
+        (
+            {
+                "field_1": 10,
+                "field_2": "str_value",
+                "field_3": "garbage",
+            },
+            TestRecord(field_1=10, field_2="str_value"),
+        ),
+    ],
+)
 def test_base_record_from_dict(data_dict: dict, expected_model: TestRecord):
     assert TestRecord.from_dict(data_dict) == expected_model
 
 
-@pytest.mark.parametrize('model,expected_dict', [
-    (
-        TestRecord(field_1=10, field_2='str_value'),
-        {
-            'field_1': 10,
-            'field_2': 'str_value',
-        },
-    ),
-])
+@pytest.mark.parametrize(
+    "model,expected_dict",
+    [
+        (
+            TestRecord(field_1=10, field_2="str_value"),
+            {
+                "field_1": 10,
+                "field_2": "str_value",
+            },
+        ),
+    ],
+)
 def test_base_record_to_dict(model: TestRecord, expected_dict: dict):
     assert model.to_dict() == expected_dict
---
+++ tests/test_system_configuration.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import pytest
+
+from user_manager.api.base_plugins.base_output_plugin import BaseOutputPlugin
+from user_manager.api.system_configuration import SystemConfiguration
+
+
+def test_system_configuration_default():
+    system_configuration = SystemConfiguration()
+
+    assert system_configuration.get_output_cls().NAME == "simple"
+    assert system_configuration.get_record_cls().NAME == "user-record"
+    assert system_configuration.get_storage_cls().NAME == "json"
+
+
+def test_system_configuration_extra_plugins(plugins_dir: Path):
+    system_configuration = SystemConfiguration(plugin_folders=[plugins_dir])
+
+    assert system_configuration.get_output_cls().NAME == "simple"
+    assert system_configuration.get_record_cls().NAME == "user-record"
+    assert system_configuration.get_storage_cls().NAME == "json"
+
+    system_configuration.set_output_implementation_name("module2-class1")
+
+    assert system_configuration.get_output_cls().NAME == "module2-class1"
+    assert system_configuration.get_record_cls().NAME == "user-record"
+    assert system_configuration.get_storage_cls().NAME == "json"
+
+    with pytest.raises(RuntimeError) as err:
+        system_configuration._get_default_implementation_cls(BaseOutputPlugin)
+
+    assert "must be called" in str(err.value)
